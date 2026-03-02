a3095ad9fa2b0697437f08c8c76a1eb852b3a9ff
Abhinav Anand <abhinavanandbth@gmail.com>
2026-03-02 07:22:42 +0000

    datatypes

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -1,3 +1,18 @@
 "use strict"; //traet all js code as newer version
 
-alert(3+3)
+// alert(3 +3) we are using node js not browser
+console.log(3+3)
+console.log("Abhinav")
+
+let name = 'Abhinav'
+let age = 18
+let isloggedIn = false
+
+// for big no we use "bigint"
+// String => ""
+// boolean => true/false
+// null => satndalone value type
+// undefined => no any value will assign 
+// symbol => unique 
+// object
+console.log(typeof "age")
